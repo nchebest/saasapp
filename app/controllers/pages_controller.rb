e51dc8078467a3cac15bb9c0e92f212bc7368317
Nchebest <Nchebest1@gmail.com>
2018-09-05 13:07:23 +0000

    Added pages to site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
     
     def about
     end
+    
+    def contact
+    end
+    
+    def schedule
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
  root to: 'pages#home'
  
  get 'about', to: 'pages#about'
+ 
+ get 'contact', to: 'pages#contact'
+ get 'shedule', to: 'pages#shedule'
 end
