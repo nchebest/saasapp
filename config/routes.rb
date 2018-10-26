3c6e21348bac45d7b40bae542f8ca19ee84bd330
Nchebest <Nchebest1@gmail.com>
2018-10-26 13:52:00 +0000

    Added Contact form to the Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
  root to: 'pages#home'
  
  get 'about', to: 'pages#about'
- 
- get 'contact', to: 'pages#contact'
  get 'shedule', to: 'pages#shedule'
  resources :contacts
  
