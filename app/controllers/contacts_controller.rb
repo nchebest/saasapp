65e679bbb0055af237e3594c22cde5dd4f5e1b6e
Nchebest <Nchebest1@gmail.com>
2018-11-07 14:57:19 +0000

    Added contact mailer view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,9 +5,9 @@ class ContactsController < ApplicationController
   def create
   @contact = Contact.new(contact_params)
   if @contact.save
-    name = params[:contact] [:name]
-    email = params[:contact] [:email]
-    body = params[:contact] [:comments]
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    body = params[:contact][:comments]
     ContactMailer.contact_email(name, email, body).deliver
     
     flash[:success] = "Message Sent!"
